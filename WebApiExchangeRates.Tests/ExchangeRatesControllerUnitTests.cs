95859daf9a5dc8b1dc8de75d3dd6c26885e8c21e
clortus <clortus13@gmail.com>
2022-09-29 10:18:26 +0300

    unit test bug fix
    change ValidCurrency from GBR to GBP

--- WebApiExchangeRates.Tests/ExchangeRatesControllerUnitTests.cs
+++ WebApiExchangeRates.Tests/ExchangeRatesControllerUnitTests.cs
@@ -1,11 +1,8 @@
 using NUnit.Framework;
 using RestSharp;
 using System.Net;
-using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
-using Microsoft.AspNetCore.Mvc;
-using NUnit.Framework.Internal;
-
+using WebApiExchangeRates.Models;
 
 namespace WebApiExchangeRates.Tests
 {
@@ -23,14 +20,14 @@ namespace WebApiExchangeRates.Tests
 
         private static readonly string ValidDate = "2022-09-22";
         private static readonly string InvalidDate = "2022-10-22";
-        private static readonly string ValidCurrency = "GBR";
+        private static readonly string ValidCurrency = "GBP";
         private static readonly string InvalidCurrency = "RUB";
 
         [Test]
         public void GetAll_InvalidDate_BadRequest()
         {
 	        // arrange
-	        RestRequest request = new($"GetAll?date={InvalidDate}", Method.Get);
+	        RestRequest request = new($"GetAllByDate?date={InvalidDate}", Method.Get);
 
 	        // act
 	        RestResponse response = Client.Execute(request);
@@ -44,7 +41,7 @@ namespace WebApiExchangeRates.Tests
         public void Get_InvalidDate_BadRequest()
         {
 	        // arrange
-	        RestRequest request = new($"Get?date={InvalidDate}&currencyStr={ValidCurrency}", Method.Get);
+	        RestRequest request = new($"GetByDateCurrency?date={InvalidDate}&currencyStr={ValidCurrency}", Method.Get);
 
 	        // act
 	        RestResponse response = Client.Execute(request);
@@ -58,7 +55,7 @@ namespace WebApiExchangeRates.Tests
         public void Get_InvalidCurrency_BadRequest()
         {
 	        // arrange
-	        RestRequest request = new($"Get?date={ValidDate}&currencyStr={InvalidCurrency}", Method.Get);
+	        RestRequest request = new($"GetByDateCurrency?date={ValidDate}&currencyStr={InvalidCurrency}", Method.Get);
 
 	        // act
 	        RestResponse response = Client.Execute(request);
@@ -68,7 +65,21 @@ namespace WebApiExchangeRates.Tests
 	        Assert.That(array["statusCode"].ToString(), Is.EqualTo(HttpStatusCode.BadRequest.GetHashCode().ToString()));
         }
 
-        //Ќе уверен, стоит ли провер€ть стоимость валют, что она находитс€ в необходимых по задаче диапазонах 
+        [Test]
+        public void Get_ValidDateValidCurrency_Deserialisable()
+        {
+            // arrange
+            RestRequest request = new($"GetByDateCurrency?date={ValidDate}&currencyStr={ValidCurrency}", Method.Get);
+
+            // act
+            RestResponse response = Client.Execute(request);
+
+            // assert
+            dynamic array = JsonConvert.DeserializeObject(response.Content);
+            var value = array["value"].ToString();
+            ExchangeRateInfo? exchangeRateInfo = JsonConvert.DeserializeObject<ExchangeRateInfo>(value);
+            Assert.That(exchangeRateInfo != null, Is.EqualTo(true));
+        }
 
     }
 }
